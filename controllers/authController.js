ca4b0f59b5db777a0bc7be0027eee974b454d36c
Chasith-Randima <chasith19@gmail.com>
2022-05-09 11:30:02 +0530

    created sign up and login functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const AppError = require("./../utils/appError");
 const User = require("./../models/userModel");
 const jwt = require("jsonwebtoken");
 
-// auth functions
+// authentipication functions
 signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
